dfed6b4a25140d754164ab86aef7a18e0df053a7
JosephKorivi <joseph.korivi0911@gmail.com>
2023-12-20 11:59:26 +0530

    done

---
+++ Loan Calc/script.js
@@ -0,0 +1,25 @@
+// Update the button id to match the HTML code
+document.getElementById('calculateBtn').addEventListener('click', calculateLoan);
+
+function calculateLoan() {
+    const loanAmount = parseFloat(document.getElementById("loanAmountInput").value);
+    const interestRate = parseFloat(document.getElementById("interestRateInput").value);
+    const loanTerm = parseFloat(document.getElementById("loanTermInput").value);
+
+    if(isNaN(loanAmount) || isNaN(interestRate) || isNaN(loanTerm)) {
+        alert("Please Enter Valid Numbers for all the Fields");
+        return; // Added a return statement to exit the function early
+    }
+
+    const monthlyInterest = interestRate / 100 / 12;
+    const totalPayments = loanTerm;
+    const monthlyPayment = (loanAmount * monthlyInterest) / (1 - Math.pow(1 + monthlyInterest, -totalPayments));
+    const totalInterest = (monthlyPayment * totalPayments) - loanAmount;
+    displayResult(monthlyPayment, totalInterest);
+}
+
+function displayResult(monthlyPayment, totalInterest) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `<p><strong>Monthly Payment : ${monthlyPayment.toFixed(2)}</strong></p>
+                            <p><strong>Total Interest : ${totalInterest.toFixed(2)}</strong></p>`;
+}
